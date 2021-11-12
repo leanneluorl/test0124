f4c7e0fc61d6b7f9e14402f70328291825f6866d
leanneluorl <leanne.front@gmail.com>
2021-11-12 17:06:20 +0800

    F2E nav....

---
+++ src/components/Nav.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="nav-box">
+        <router-link v-for="item in navItem"
+            :to="item.link" :key="item.key"
+            v-slot="{ href, route, navigate, isActive }">
+            <div class="nav-item" :class="{ 'active': isActive }">
+                <a class="nav-link" @click="navigate">
+                    <span class="text">{{ $t(`HomeContent.nav.${item.key}`) }}</span>
+                </a>
+            </div>
+        </router-link> 
+    </div>
+</template>
+
+<script>
+export default {
+    data: {
+        navItem:[
+        {
+            link: "/info"
+        },
+        {   
+            link: "/sight"
+        },
+        {
+            link: "/food"
+        },
+        {
+            link: "/accommodation"
+        },
+        {
+            link: "/festival"
+        }],
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import axios from 'axios'
 
 const routes = [
   {
